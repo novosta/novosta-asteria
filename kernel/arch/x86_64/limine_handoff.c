93e706f8b4231ecdc1d54c3261f05e9476845a8d
Jacob <39503993+novosta@users.noreply.github.com>
2025-08-17 20:56:45 -0700

    fix: include limine.cfg at ISO root

--- kernel/arch/x86_64/limine_handoff.c
+++ kernel/arch/x86_64/limine_handoff.c
@@ -1,7 +1,7 @@
 #include <novosta/limine.h>
 
-__attribute__((section(".limine_reqs")))
-struct limine_framebuffer_request framebuffer_request = {
+__attribute__((section(".limine_reqs"), used))
+static volatile struct limine_framebuffer_request framebuffer_request = {
     .id = LIMINE_FRAMEBUFFER_REQUEST,
     .revision = 0
 };
